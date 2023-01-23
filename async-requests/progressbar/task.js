456c324d11477a155a7c055454f2995bae637ddf
Dmitrii Bulatov <bulati484@gmail.com>
2023-01-23 20:48:28 +0300

    second commit

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,39 +1,16 @@
-const form = document.querySelector( '#form' );
-const uploadFile = document.querySelector( '#file' );
-const btnInput = document.querySelectorAll('span')[0]
-const nameOfFile = document.querySelectorAll('span')[1];
-const progressBar = document.getElementById( 'progress' );
+const formEl = document.getElementById('form');
+const progressEl = document.getElementById('progress');
 
-btnInput.onclick = function() {
-  progressBar.value = 0;
-}
-
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const file = uploadFile.files[0];
-  const formSent = new FormData();
-  const xhr = new XMLHttpRequest();
-  
-  if (uploadFile.files.length > 0) {
-
-    formSent.append('form', file);
-    
-    xhr.upload.addEventListener('progress', progressHandler, false);
-    xhr.addEventListener('load', loadHandler, false);
-    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
-    xhr.send(formSent);
-  } else {
-    alert('Сначала выберите файл')
-  }
-  return false
+formEl.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    const request = new XMLHttpRequest();
+    request.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    const file = document.getElementById('file').files;
+    const fileName = document.querySelector('.input__wrapper-desc');
+    const formData = new FormData();
+    formData.append(`${fileName.innerText}`, file[0]);
+    request.upload.addEventListener('progress', function (ev){
+        progressEl.value = (ev.loaded / ev.total).toFixed(2);
+    }, false);
+    request.send(formData);
 });
-
-
-function progressHandler(event) {
-  const percentLoaded = Math.round((event.loaded / event.total) * 100);
-  progressBar.value = percentLoaded;
-}
-
-function loadHandler() {
-  progressBar.value = 0;
-}
