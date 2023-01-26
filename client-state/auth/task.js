1e8d46ff55ed64e986a45456805d2a07d579e84a
Dmitrii Bulatov <bulati484@gmail.com>
2023-01-26 21:18:35 +0300

    final commit

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -2,41 +2,43 @@ const form = document.getElementById('signin__form');
 const welcome = document.querySelector('#welcome');
 document.querySelector('#signin').classList.add('signin_active');
 
-
 form.addEventListener('submit', (ev) => {
   ev.preventDefault();
   const formSent = new FormData(form); 
   const request = new XMLHttpRequest();
-  
+
   request.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
   request.send(formSent);
   request.addEventListener('readystatechange', function() {
-    if (request.readyState === request.DONE && request.status === 200) {
+    if (request.readyState === request.DONE && request.status === 201) {
       const data = JSON.parse(request.responseText);
-      document.querySelector('#signin').classList.remove('signin_active');
-      welcome.classList.add('welcome_active');
-      welcome.querySelector('span').innerHTML = data.user_id;
-    } else {
-      // popUpMsg();
-      alert('Неверный логин/пароль');
+
+      if (!data.user_id) {
+        popUpMsg();
+        document.querySelectorAll('.control').forEach( ev =>  {
+          ev.value = '';
+        })      
+      } else {
+        document.querySelector('#signin').classList.remove('signin_active');
+        welcome.classList.add('welcome_active');
+        welcome.querySelector('span').innerHTML = data.user_id;
+      }
     }
   });
 })
 
-
-
-// function popUpMsg() {
-//   const popUpMsg = document.querySelector('body');
-//   popUpMsg.insertAdjacentHTML("beforeEnd",
-//     `<div class="modal_mask">
-//        <div class="modal">
-//             <div class="modal_msg">Неверный логин/пароль</div>
-//             <button class="close_btn">Закрыть</button>
-//         </div>
-//     </div>`
-//   );
-//   const closeBtn = document.querySelector('.close_btn');
-//   closeBtn.addEventListener('click', (ev) => {
-//     ev.target.closest('div.modal_mask').remove();
-//   });
-// }
+function popUpMsg() {
+  const popUpMsg = document.querySelector('body');
+  popUpMsg.insertAdjacentHTML("beforeEnd",
+    `<div class="modal_mask">
+        <div class="modal">
+          <div class="modal_msg">Неверный логин/пароль</div>
+            <button class="close_btn">Закрыть</button>
+          </div>
+    </div>`
+  );
+  const closeBtn = document.querySelector('.close_btn');
+  closeBtn.addEventListener('click', (ev) => {
+    ev.target.closest('div.modal_mask').remove();
+  });
+}
